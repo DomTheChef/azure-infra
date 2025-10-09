51fbd9ce40000300710331f06c6a979be888050a
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-09 13:56:18 +0200

    fix: fixed references to azure_rm -> azurerm so terraform stops trying to fetch hashicorp/azure instead of hashicorp/azurerm

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,4 +1,4 @@
-resource "azure_rm_resource_group" "this" {
+resource "azurerm_resource_group" "this" {
     name     = "dom-web-app-api-test-rg"
     location = "westeurope"
 }
--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,7 +1,7 @@
 output "name" {
-    value = azure_rm_resource_group.this.name
+    value = azurerm_resource_group.this.name
 }
 
 output "id" {
-    value = azure_rm_resource_group.this.id
+    value = azurerm_resource_group.this.id
 }
